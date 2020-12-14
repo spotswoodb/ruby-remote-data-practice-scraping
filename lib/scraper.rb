4cc6214d0574cd42d9aa6a6174976ffa953015fa
Ben Spotswood <spotswoodb@gmail.com>
2020-12-14 10:45:02 -0600

    commit tree 4f04307639846beb24b3265542b05dee67eabedb

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,10 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+
+courses = doc.css(".tout__label.heading.heading--level-4")
+
+courses.each do |course|
+  puts course.text.strip
+end 
